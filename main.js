0b791a44bfdef781982c357b03db2cdf68fbb715
Glenn Kamau Mwangi <glennkamau2@gmail.com>
2022-06-23 17:37:53 +0300

    changes to main.js

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ let fruitTitles = () => {
 
     })
   })
-  .catch(err => console.error(err));
+  .catch(err => console.error(err))
   }
 
   let getFruits = () =>{
@@ -55,30 +55,30 @@ let fruitTitles = () => {
   }  
   initialize()
 
-const navbar = document.querySelector(".navbar");
-const hamburger = document.querySelector(".hamburger");
+const navbar = document.querySelector(".navbar")
+const hamburger = document.querySelector(".hamburger")
 
 hamburger.addEventListener("click", () => {
-  navbar.classList.toggle("show");
-});
+  navbar.classList.toggle("show")
+})
 
-const header = document.querySelector('.header');
-const scrollLink = document.querySelectorAll('.navbar a:not(:last-child)');
+const header = document.querySelector('.header')
+const scrollLink = document.querySelectorAll('.navbar a:not(:last-child)')
 
 Array.from(scrollLink).map((link) => {
   link.addEventListener('click', (e) => {
     // Prevent Default
-    e.preventDefault();
+    e.preventDefault()
 
-    const id = e.currentTarget.getAttribute('href').slice(1);
-    const element = document.getElementById(id);
-    let position = element.offsetTop - 90;
+    const id = e.currentTarget.getAttribute('href').slice(1)
+    const element = document.getElementById(id)
+    let position = element.offsetTop - 90
 
     window.scrollTo({
       left: 0,
       top: position,
       behavior: 'smooth',
     });
-    navbar.classList.remove('show');
+    navbar.classList.remove('show')
   });
 });
